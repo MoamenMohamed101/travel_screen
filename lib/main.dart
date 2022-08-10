b7e6b52b36808f6dc4fc4c1a05eff543c4ed85ab
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-08-10 23:21:35 +0200

    travel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_screen/fadeAnimation.dart';
 
 void main() {
   runApp(
@@ -10,7 +11,7 @@ void main() {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -21,103 +22,198 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 300,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/background.jpg'),
-                fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 300,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomRight,
-                  colors: [
-                    Colors.black.withOpacity(.8),
-                    Colors.black.withOpacity(.2),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.2),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    FadeAnimation(
+                      1,
+                      const Text(
+                        'What you would like to find?',
+                        //textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    FadeAnimation(
+                      1.3,
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 3),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        margin: const EdgeInsets.symmetric(horizontal: 40),
+                        height: 50,
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.search, color: Colors.grey),
+                            hintText: 'Search for cities, places',
+                            hintStyle:
+                                TextStyle(fontSize: 15, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
                   ],
                 ),
               ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              //padding: EdgeInsets.symmetric(horizontal: 20),
+              margin: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
-                    'What you would like to find?',
-                    //textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+                  FadeAnimation(
+                    1,
+                    Text(
+                      'Best Destination',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                   const SizedBox(
-                    height: 30,
+                    height: 20,
                   ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 3),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    margin: const EdgeInsets.symmetric(horizontal: 40),
-                    height: 50,
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
-                        hintText: 'Search for cities, places',
-                        hintStyle: TextStyle(fontSize: 15, color: Colors.grey),
+                  FadeAnimation(
+                    1.4,
+                    SizedBox(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          makeItem('assets/images/canada.jpg', 'Canada'),
+                          makeItem('assets/images/greece.jpg', 'Greece'),
+                          makeItem('assets/images/Italy.jpg', 'Italy'),
+                          makeItem('assets/images/united-states.jpg',
+                              'United States'),
+                        ],
                       ),
                     ),
                   ),
                   const SizedBox(
                     height: 30,
                   ),
+                  // SizedBox(
+                  //   height: 50,
+                  //   child: ListView(
+                  //     scrollDirection: Axis.horizontal,
+                  //     children: [
+                  //       AspectRatio(
+                  //         aspectRatio: 1.5 / 1,
+                  //         child: Container(
+                  //           decoration: BoxDecoration(
+                  //             borderRadius: BorderRadius.circular(15),
+                  //             color: Colors.blue.withOpacity(.2),
+                  //           ),
+                  //           margin: const EdgeInsets.only(right: 15),
+                  //           child: const Icon(Icons.hotel, color: Colors.blue),
+                  //         ),
+                  //       ),
+                  //       AspectRatio(
+                  //         aspectRatio: 1.5 / 1,
+                  //         child: Container(
+                  //           decoration: BoxDecoration(
+                  //             borderRadius: BorderRadius.circular(15),
+                  //             color: Colors.white,
+                  //           ),
+                  //           margin: const EdgeInsets.only(right: 15),
+                  //           child: const Icon(Icons.flight, color: Colors.red),
+                  //         ),
+                  //       ),
+                  //       AspectRatio(
+                  //         aspectRatio: 1.5 / 1,
+                  //         child: Container(
+                  //           decoration: BoxDecoration(
+                  //             borderRadius: BorderRadius.circular(15),
+                  //             color: Colors.white,
+                  //           ),
+                  //           margin: const EdgeInsets.only(right: 15),
+                  //           child: const Icon(Icons.event, color: Colors.green),
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  // const SizedBox(
+                  //   height: 20,
+                  // ),
+                  FadeAnimation(
+                    1,
+                    Text(
+                      'Best Hotels',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                    1.4,
+                    SizedBox(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          makeItem('assets/images/canada.jpg', 'Canada'),
+                          makeItem('assets/images/greece.jpg', 'Greece'),
+                          makeItem('assets/images/Italy.jpg', 'Italy'),
+                          makeItem('assets/images/united-states.jpg',
+                              'United States'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            //padding: EdgeInsets.symmetric(horizontal: 20),
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Best Destination',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey[800],
-                    fontSize: 20,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 200,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      makeItem('assets/images/canada.jpg', 'Canada'),
-                      makeItem('assets/images/greece.jpg', 'Greece'),
-                      makeItem('assets/images/Italy.jpg', 'Italy'),
-                      makeItem(
-                          'assets/images/united-states.jpg', 'United States'),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
